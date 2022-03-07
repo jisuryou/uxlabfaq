57367426e66e41a9f56a6f4fff7c5b90d292c31b
jisuryou <alji7@naver.com>
2022-03-07 11:21:10 +0900

    no message

---
+++ bot/Bots/QnABot.cs
@@ -0,0 +1,52 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.5.0
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Schema;
+
+namespace Microsoft.BotBuilderSamples.Bots
+{
+    public class QnABot<T> : ActivityHandler where T : Microsoft.Bot.Builder.Dialogs.Dialog
+    {
+        protected readonly BotState ConversationState;
+        protected readonly Microsoft.Bot.Builder.Dialogs.Dialog Dialog;
+        protected readonly BotState UserState;
+
+        public QnABot(ConversationState conversationState, UserState userState, T dialog)
+        {
+            ConversationState = conversationState;
+            UserState = userState;
+            Dialog = dialog;
+        }
+
+        public override async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default)
+        {
+            await base.OnTurnAsync(turnContext, cancellationToken);
+
+            // Save any state changes that might have occurred during the turn.
+            await ConversationState.SaveChangesAsync(turnContext, false, cancellationToken);
+            await UserState.SaveChangesAsync(turnContext, false, cancellationToken);
+        }
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken) =>
+            // Run the Dialog with the new message Activity.
+            await Dialog.RunAsync(turnContext, ConversationState.CreateProperty<DialogState>(nameof(DialogState)), cancellationToken);
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"안녕하세요 UX lab 연구실 챗봇입니다. 챗봇이 아직 성장 중이라 정확하지 않은 답변이 있을 수도 있어요! 해결되지 않은 질문들은 이메일을 통해 문의주세요!"), cancellationToken);
+                }
+            }
+        }
+    }
+}
---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABotAllFeatures.sln -p:DeployOnBuild=true -p:PublishProfile=.\uxlabfaq2-bot-8bdc-Web-Deploy.pubxml -p:Password=gBvXNrsRl9srd5hHq4wjqWJQq9vwcCrG70QkjrG7voGuNilmsttN27EEdgr0
+
